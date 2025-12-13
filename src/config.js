413ff17629a488dbf86bba1e3f742ff74a598cf5
BG2FOU <111550009+BG2FOU@users.noreply.github.com>
2025-12-14 00:50:02 +0800

    Delete src/config.js

--- src/config.js
+++
@@ -1,8 +0,0 @@
-// 临时 API 配置文件
-// 生产环境时，将此文件从 .gitignore 中取消忽略，或通过环境变量替换
-const CONFIG = {
-    AMAP_API_KEY: 'e5b759a2a9a743ac99a07e48b1dbe23b',
-    AMAP_SECURITY_JS_CODE: 'f93d8688a469db5a5645503baba74a60'
-};
-
-// 使用方式：在需要 API Key 的地方使用 CONFIG.AMAP_API_KEY
